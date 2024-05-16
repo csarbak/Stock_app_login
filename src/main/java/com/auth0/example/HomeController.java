8640252bd9eaef5d27e3c6f30c5395035c63c20c
conradscomputer <sarbakc@gmail.com>
2024-05-16 01:25:56 -0400

    login

--- src/main/java/com/auth0/example/HomeController.java
+++ src/main/java/com/auth0/example/HomeController.java
@@ -1,24 +1,32 @@
 package com.auth0.example;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller for the home page.
  */
-@Controller
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class HomeController {
-
     @GetMapping("/")
-    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
-        if (principal != null) {
-            model.addAttribute("profile", principal.getClaims());
-        }
-        System.out.println( "MODEL: \n:" + model.asMap().toString());
-        System.out.println( "PRINCPLE: \n:" +principal.getClaims().toString() );
-        return "index";
+    public ResponseEntity<?> home(Object o) {
+        System.out.println("object : "+ o.toString());
+//        if (principal != null) {
+//            model.addAttribute("profile", principal.getClaims());
+//            System.out.println( "PRINCPLE: \n:" +principal.getClaims().toString() );
+//
+//        }
+//        System.out.println( "MODEL: \n:" + model.asMap().toString());
+//        System.out.println( "PRINCPLE: \n:" +principal.getClaims().toString() );
+        return ResponseEntity.ok().body("controller");
     }
 }
